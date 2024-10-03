37c58487cc1dab96c883b5fc541a98e0e843b29b
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-03 18:06:07 +0100

    add favourite page to app

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
  root "cars#index"
   resources :cars, only: [ :index, :show ] do
     resources :reviews, only: [ :create ]
+    resources :favourites, only: [ :create ]
   end
+  resources :favourites, only: [ :index, :destroy ]
 end
