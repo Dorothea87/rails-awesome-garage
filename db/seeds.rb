ac501ca38151eca147ef8baf695cd1d37fc8853e
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-03 14:54:17 +0100

    create data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ owner3 = Owner.create!(nickname: "car_tuner65")
 
 puts "Creating cars..."
 
-Car.create!{brand: "Volkswagen", model: "Polo", year: "2014", fuel: "unleaded petrol", owner: owner3}
-Car.create!{brand: "MINI", model: "Cooper", year: "2024", fuel: "electric", owner: owner2}
-Car.create!{brand: "Ferrari", model: "Roma", year: "2020", fuel: "unleaded petrol", owner: owner1}
+Car.create!(brand: "Volkswagen", model: "Polo", year: "2014", fuel: "unleaded petrol", owner: owner3)
+Car.create!(brand: "MINI", model: "Cooper", year: "2024", fuel: "electric", owner: owner2)
+Car.create!(brand: "Ferrari", model: "Roma", year: "2020", fuel: "unleaded petrol", owner: owner1)
 
 puts "Finished! #{Owner.all.count} owners created."
