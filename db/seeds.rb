35f760cca7db4f9fb64728a89166ab900dc72694
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-03 11:09:34 +0100

    add models with associations and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Car.destroy_all
+
+puts "Creating cars..."
+polo = {brand: "Volkswagen", model: "Polo", year: "2014", fuel: "unleaded petrol"}
+mini = {brand: "MINI", model: "Cooper", year: "2024", fuel: "electric"}
+ferrari = {brand: "Ferrari", model: "Roma", year: "2020", fuel: "unleaded petrol"}
+
+[polo, mini, ferrari].each do |attributes|
+  car = Car.create!(attributes)
+  puts "Created #{car.name}"
+end
+puts "Finished!"
